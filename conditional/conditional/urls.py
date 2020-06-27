820f0bb2201cb91197fbe7e139a5c335f7a0044c
Max Meinhold <mxmeinhold@gmail.com>
2020-06-27 00:42:12 -0400

    Fix variable names
    schema_view -> SchemaView

--- conditional/conditional/urls.py
+++ conditional/conditional/urls.py
@@ -18,7 +18,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(
+SchemaView = get_schema_view(
     openapi.Info(
         title='Conditional API',
         default_version='v2',
@@ -31,8 +31,8 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    re_path(r'^api/docs(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^api/docs/?$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^api/docs(?P<format>\.json|\.yaml)$', SchemaView.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^api/docs/?$', SchemaView.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
     re_path(r'^api/attendance/?', include('attendance.urls')),
     re_path(r'^api/evaluations/?', include('evaluations.urls')),
