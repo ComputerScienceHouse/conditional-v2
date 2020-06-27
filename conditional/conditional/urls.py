73c0690ae51746d645db1d7d282caa3ae6928b62
Max Meinhold <mxmeinhold@gmail.com>
2020-06-27 01:19:41 -0400

    Remove all unused imports - TODO temporary, revert
    Most of these probably should be used, they just aren't yet and I want
    to see a pretty travis passing mark

--- conditional/conditional/urls.py
+++ conditional/conditional/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include, re_path
+from django.urls import include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
