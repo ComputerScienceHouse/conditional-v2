67695f4d3bdab85b07647ef5e995d8f02cf5863e
Devin Matte <devinmatte@gmail.com>
2020-08-23 18:36:57 -0400

    Fixing typescript

--- conditional/members/views.py
+++ conditional/members/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from member.serializers import MemberAccountSerializer
 
-# Create your views here.
+class MemberListView(viewsets.ViewSet):
+    serializer_class = serializers.MemberAccountSerializer
